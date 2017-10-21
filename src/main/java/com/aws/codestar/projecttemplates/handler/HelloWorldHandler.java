23bf3a9efe1fb7d48825b611f246c39312c612b5
SID <SID@SID-PC>
2017-10-21 13:41:25 -0400

    getting queryStringParameters

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -1,11 +1,13 @@
 package com.aws.codestar.projecttemplates.handler;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.GatewayResponse;
 
 /**
@@ -13,11 +15,30 @@ import com.aws.codestar.projecttemplates.GatewayResponse;
  */
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 
-    public Object handleRequest(final Object input, final Context context) {
-    	context.getLogger().log("input: " + input + "\n");
-    	context.getLogger().log("input.getClass(): " + input.getClass() + "\n");
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return new GatewayResponse("{ \"Output\": \"Hello World!\"}", headers, 200);
-    }
+	public Object handleRequest(final Object input, final Context context) {
+
+		context.getLogger().log("Input to the Lambda: " + input + "\n");
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
+		try {
+			if (input instanceof LinkedHashMap) {
+				@SuppressWarnings("unchecked")
+				LinkedHashMap<Object, Object> calculaionParam = (LinkedHashMap<Object, Object>) input;
+				context.getLogger().log("calculaionParam: " + calculaionParam.get("queryStringParameters") + "\n");
+				context.getLogger().log(
+						"calculaionParam getClass: " + calculaionParam.get("queryStringParameters").getClass() + "\n");
+				headers.put("Content-Type", "application/json");
+				
+				StringBuilder response = new StringBuilder();
+				response.append("{ \"Output\": \"" + calculaionParam.get("queryStringParameters") + "\"}");
+				return new GatewayResponse(response.toString(), headers, HttpStatus.OK.value());
+			}
+			context.getLogger().log("Incorrect input type: " + input.getClass());
+			return new GatewayResponse("{ \"Output\": \"Bad Request!\"}", headers, HttpStatus.BAD_REQUEST.value());
+		} catch (Throwable e) {
+			context.getLogger().log("Exception Encountered.");
+			return new GatewayResponse("{ \"Output\": \"Exception Encountered!\"}", headers,
+					HttpStatus.INTERNAL_SERVER_ERROR.value());
+		}
+	}
 }
